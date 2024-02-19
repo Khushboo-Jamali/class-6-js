da85d8c33c46804cefc8f7695fc10b431742424a
khushboo-jamali <jamalikhushboo@gmail.com>
2024-02-19 22:52:34 +0100

    start

---
+++ app.js
@@ -0,0 +1,18 @@
+let a = prompt();
+let b = "milk";
+let f = "Cadbury Dairy Milk Chocolate Silk Price in Pakistan is  ";
+let c = "Rs : ";
+let d = 190;
+let e = 80;
+
+if (a == b) {
+  document.write(
+    "<input type='text'  placeholder=' Search Dary Milk'  > ",
+    f,
+    c,
+    d + e
+  );
+  console.log(f, c, d + e);
+} else {
+  document.write("invalid");
+}
